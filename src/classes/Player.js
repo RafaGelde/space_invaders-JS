d361ec3f388ea7a0d6a84c40ab0ce8f6b8d35ac6
Rafael Gelde <rafaelgelde@gmail.com>
2026-03-09 23:14:57 -0300

    criando a classe projectiles e adicionando imagem e animações ao player

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,28 +1,93 @@
+import { 
+    INITIAL_FRAMES,
+    PATH_ENGINE_IMAGE,
+    PATH_ENGINE_SPRITES,
+    PATH_SPACESHIP_IMAGE
+} from "../utils/constants.js";
+import Projectile from "./Projectile.js";
+
 class Player {
     constructor(canvasWidth, canvasHeight) {
-        this.width = 100;
-        this.height = 100;
-        this.velocity = 5;
+        this.width = 48 * 2;
+        this.height = 48 * 2;
+        this.velocity = 8;
         
         this.position = {
             x : canvasWidth / 2 - this.width / 2,
             y : canvasHeight - this.height - 30,
         };
+
+        this.image = this.getImage(PATH_SPACESHIP_IMAGE);
+        this.engineImage = this.getImage(PATH_ENGINE_IMAGE);
+        this.engineSprite = this.getImage(PATH_ENGINE_SPRITES);
+
+        this.sx = 0;
+        this.framesCounter = INITIAL_FRAMES;
+    }
+
+    getImage(path) {
+        const image = new Image();
+        image.src = path;
+        return image;
     }
 
     moveLeft() {
         this.position.x -= this.velocity;
     }
+
     moveRight() {
         this.position.x += this.velocity;
     }
+
     draw(ctx) {
-        ctx.fillStyle = "red";
-        ctx.fillRect(
+
+        ctx.drawImage(
+            this.image, 
             this.position.x, 
             this.position.y, 
             this.width, 
-            this.height);
+            this.height
+        );
+
+        ctx.drawImage(
+            this.engineSprite,
+            this.sx, 
+            0, 
+            48, 
+            48,
+            this.position.x, 
+            this.position.y + 4, 
+            this.width, 
+            this.height
+        );
+
+        ctx.drawImage(
+            this.engineImage, 
+            this.position.x, 
+            this.position.y + 3, 
+            this.width, 
+            this.height
+        );
+
+        this.update();
+    }
+    update() {
+        if (this.framesCounter === 0) {
+            this.sx = this.sx === 96 ? 0 : this.sx + 48
+            this.framesCounter = INITIAL_FRAMES;    
+        }
+
+        this.framesCounter--;
+    }
+
+    shoot(projectiles) {
+        const p = new Projectile({
+            x: this.position.x + this.width / 2 - 0.5,
+            y: this.position.y + 2 + 1,
+        },
+        -10
+    );
+    projectiles.push(p);
     }
 }
 
--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import Player from "./classes/Player.js";
+import Projectile from "./classes/Projectile.js";
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -6,24 +7,70 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+ctx.imageSmoothingEnabled = false;
+
 const player = new Player(canvas.width, canvas.height);
+const playerProjectiles = [];
 
 const keys = {
     left: false,
     right: false,
+    shoot: {
+        pressed: false,
+        released: true
+    },
+}
+
+const drawnProjectiles = () => {
+    playerProjectiles.forEach((projectile) => {
+        projectile.draw(ctx);
+        projectile.update();
+    })
+}
+
+const clearProjectiles = () => {
+    playerProjectiles.forEach((projectile, index) => {
+        if (projectile.position.y <= 0) {
+            playerProjectiles.splice(index, 1);
+        }
+    })
 }
 
 const gameloop = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    drawnProjectiles();
+    clearProjectiles();
+
+    ctx.save(ctx);
+
+    ctx.translate(
+        player.position.x + player.width / 2, 
+        player.position.y + player.height / 2
+    );
+
+    if (keys.shoot.pressed && keys.shoot.released) {
+        player.shoot(playerProjectiles);
+        keys.shoot.released = false;
+    }
+
     if (keys.left && player.position.x >= 0) {
         player.moveLeft();
+        ctx.rotate(-0.15);
     }
     if (keys.right && player.position.x <= canvas.width - player.width) {
         player.moveRight();
+        ctx.rotate(0.15);
     }
 
+    ctx.translate(
+        - player.position.x - player.width / 2, 
+        - player.position.y - player.height / 2
+    );
+
     player.draw(ctx);
+
+    ctx.restore();
     
     requestAnimationFrame(gameloop);
 }
@@ -35,6 +82,7 @@ addEventListener("keydown", (event) => {
 
     if (key === "a") keys.left = true;
     if (key === "d") keys.right = true;
+    if (key === "enter") keys.shoot.pressed = true;
 })
 
 addEventListener("keyup", (event) => {
@@ -42,6 +90,10 @@ addEventListener("keyup", (event) => {
 
     if (key === "a") keys.left = false;
     if (key === "d") keys.right = false;
+    if (key === "enter") {
+        keys.shoot.pressed = false;
+        keys.shoot.released = true;
+    }
 })
 
 gameloop();
